442332fe87cce892704339997259399c98a718c5
Anderson Walsh <andy@Andersons-MacBook-Air.local>
2021-11-02 14:51:59 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+'''
+Andy W
+8/31
+data types
+'''
+'''
+my_string="hello world"
+x = 10
+
+number = 8.8
+
+print (type (x))
+print (type (my_string))
+print (type (number))
+
+result = (number * x)
+print (result)
+print (type (result))
+'''
+
+'''
+print ("please give me a number")
+user_number = input ()
+
+print (user_number)
+print (type(user_number))
+
+did_math = (int(user_number) * 7)
+print (did_math)
+'''
+'''
+first = ""
+last = ""
+age = 0.0
+grade = 11
+
+print("what is your first name?")
+first = input()
+print("hello " + first)
+print("what is your last name?")
+last = input()
+print("hello " + first + " " + last)
+print("how old are you?")
+age = input()
+print(first + " " + last + " " + "..." + " " + "age =" + " " + age)
+print("what is your grade?")
+grade = input()
+print(first + " " + last + " " + "..." + " " + "age =" + " " + age + " " + "... " + "grade =" + " " + grade)
+print("Thank you" + " " + first + " " + last + "!")
+'''
+
